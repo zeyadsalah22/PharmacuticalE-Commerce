2496d15c1885ba47bd2f6e2de3e83ee94a28a8dd
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-09-25 16:05:19 +0300

    Add project files.

---
+++ PharmacuticalE-Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PharmacuticalE_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
