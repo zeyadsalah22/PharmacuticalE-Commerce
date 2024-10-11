7962deec061dd23b6788a61573cfe206f1ae1300
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-10-11 12:24:39 +0300

    prepare product attribute

---
+++ PharmacuticalE-Commerce/Repositories/Interfaces/IPAVRepository.cs
@@ -0,0 +1,10 @@
+﻿using PharmacuticalE_Commerce.Models;
+
+namespace PharmacuticalE_Commerce.Repositories.Interfaces
+{
+    public interface IPAVRepository : IRepository<Pav>
+    {
+        public Pav GetByCompId(int? productId, int? attributeId);
+        public void DeleteByCompId(int? productId, int? attributeId);
+    }
+}
---
+++ PharmacuticalE-Commerce/Repositories/Interfaces/IProductAttributeRepository.cs
@@ -0,0 +1,8 @@
+﻿using PharmacuticalE_Commerce.Models;
+
+namespace PharmacuticalE_Commerce.Repositories.Interfaces
+{
+    public interface IProductAttributeRepository : IRepository<ProductAttribute>
+    {
+    }
+}
