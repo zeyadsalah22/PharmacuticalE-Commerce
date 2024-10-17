273ff290b77d9eafce5bcbc595d012d9214d3e67
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-10-17 22:55:45 +0300

    fix register problem

--- PharmacuticalE-Commerce/Viewmodels/RegisterViewModel.cs
+++ PharmacuticalE-Commerce/Viewmodels/RegisterViewModel.cs
@@ -8,13 +8,13 @@ namespace PharmacuticalE_Commerce.Viewmodels
     {
         [Required]
         public string UserName { get; set; }
-        public string Fname { get; set; }
-        public string Lname { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
 		public string Email { get; set; }
         [DataType(DataType.PhoneNumber)]
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 		[Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
